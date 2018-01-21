3458a7354244ea0d1b1f0d5349e9839bf6be8c66
ramonavaz25 <rvaz@ufl.edu>
2018-01-21 06:33:08 -0500

    first commit

---
+++ SwampHacks2018_FruitOdyssey/Assets/Scripts/DrawLine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour 
+{
+
+	// Use this for initialization
+	public Material mat;
+	public GameObject start;
+	public GameObject end;
+	void Start () 
+	{
+		MeshFilter filter = new MeshFilter();
+		MeshRenderer rend = GetComponent<MeshRenderer>();
+		Mesh m = GetComponent<MeshFilter>().mesh;
+		rend.material = mat;
+		Vector3[] v = new Vector3[2];
+		v [0] = start.transform.position;
+		v[1] = end.transform.position;
+		int[] arr = new int[]{0,1};
+
+		//arr = [0,1
+		/*List<Vector3> list = */ m.SetIndices(arr, MeshTopology.Lines, 0);
+		m.vertices = v;
+
+		filter.mesh = m;
+		//rend.
+	}
+
+}
---
+++ SwampHacks2018_FruitOdyssey/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ExitScript : MonoBehaviour {
+    public Button yourButton;
+
+    void Start()
+    {
+        //Button btn = yourButton.GetComponent<Button>();
+        //btn.onClick.AddListener(TaskOnClick);
+        yourButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        Application.Quit();
+    }
+
+}
